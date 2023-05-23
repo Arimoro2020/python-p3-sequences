749c07f7d0321aacb504c9e61b846c61daefd134
Adeyemi Arimoro <arimoro71@gmail.com>
2023-05-23 08:48:37 -0500

    Add solution

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+        my_list = []
+        for i in range(length):
+            if i <= 1:
+                    my_list.append(i)
+
+            else:
+                f_minus_2 = my_list[i - 2]
+                f_minus_1 = my_list[i - 1]
+                f = f_minus_2 + f_minus_1
+                my_list.append(f)
+
+        print(my_list)
+                    
+            
+
+        
+  
+
